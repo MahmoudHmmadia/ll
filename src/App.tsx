e307d7d1f2013ea94e3dcc97390da99073da110b
Mahmoud Hmmadia <102804429+MahmoudHmmadia@users.noreply.github.com>
2023-10-30 16:25:10 +0200

    asd

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,45 @@ import Contact from "./pages/contact";
 import Companies from "./pages/companies";
 import Footer from "./components/Footer";
 import Training from "./pages/trainning";
+import { useEffect } from "react";
+import { Peer } from "peerjs";
 export const App = () => {
   const { lan } = GlobalContext();
   const location = useLocation();
+  useEffect(() => {
+    const myPeer = new Peer({
+      host: "meet-joybox.com",
+      secure: true,
+      debug: 3,
+      config: {
+        iceServers: [
+          {
+            urls: "stun:stun.relay.metered.ca:80",
+          },
+          {
+            urls: "turn:a.relay.metered.ca:80",
+            username: "976776674d5e26c0b97dd685",
+            credential: "dqwWyHCY+eCXcn8I",
+          },
+          {
+            urls: "turn:a.relay.metered.ca:80?transport=tcp",
+            username: "976776674d5e26c0b97dd685",
+            credential: "dqwWyHCY+eCXcn8I",
+          },
+          {
+            urls: "turn:a.relay.metered.ca:443",
+            username: "976776674d5e26c0b97dd685",
+            credential: "dqwWyHCY+eCXcn8I",
+          },
+          {
+            urls: "turn:a.relay.metered.ca:443?transport=tcp",
+            username: "976776674d5e26c0b97dd685",
+            credential: "dqwWyHCY+eCXcn8I",
+          },
+        ],
+      },
+    });
+  }, []);
   return (
     <div
       className={`flex flex-col gap-3 bg-gradient_main text-white overflow-hidden ${
